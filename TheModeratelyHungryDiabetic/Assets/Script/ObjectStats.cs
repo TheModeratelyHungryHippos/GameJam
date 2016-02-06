8d589af3a61e5511e0a296868ba3e293815978ac
V-FEXrt <ash123125@yahoo.com>
2016-02-06 15:27:32 -0600

    Inital gen commit

---
+++ TheModeratelyHungryDiabetic/Assets/Script/ObjectStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public enum QType{
+	Avoidable,
+	Blocking,
+	FullRoom,
+	Moving
+}
+public enum Direction{
+	None,
+	LeftRight,
+	UpDown
+}
+
+public class ObjectStats : object{
+	public string Name;
+	public int ID;
+	public QType QType;
+	public Direction Direction;
+	public int Weight;
+	public Type Type;
+
+	public ObjectStats(string name, int id, QType qType, Direction direction, int weight, Type type)
+	{
+		Name = name;
+		ID = id;
+		this.QType = qType;
+		this.Direction = direction;
+		Weight = weight;
+		this.Type = type;
+	}
+		
+}
