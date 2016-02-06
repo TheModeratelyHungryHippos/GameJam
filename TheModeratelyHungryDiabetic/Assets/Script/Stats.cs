408af65066bb74290f3926c7d1569377649f4d59
V-FEXrt <ash123125@yahoo.com>
2016-02-06 15:27:32 -0600

    Inital gen commit

---
+++ TheModeratelyHungryDiabetic/Assets/Script/Stats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Stats : object {
+
+	public static int[] GeneratedItemsPercent;
+	public static ObjectStats[] GameObjects;
+	private static bool hasBeenInit = false;
+
+	// Use this for initialization
+	public static void Init () {
+		if (hasBeenInit) {
+			return;
+		}
+		hasBeenInit = true;
+
+		// % No items, % One items, % Two items, % Three items, % Four items, % Five items
+		GeneratedItemsPercent = new int[6]{0, 0, 0, 1, 0, 0 };
+		GameObjects = new ObjectStats[3];
+		GameObjects [0] = new ObjectStats ("Wall0", 1, QType.Blocking, Direction.None, 1, typeof(Wall));
+		GameObjects [1] = new ObjectStats ("Wall1", 1, QType.Blocking, Direction.None, 2, typeof(Wall));
+		GameObjects [2] = new ObjectStats ("Wall2", 1, QType.Blocking, Direction.None, 3, typeof(Wall));
+	}
+
+}
---
+++ TheModeratelyHungryDiabetic/Assets/Script/Wall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// NOTE: THIS IS A TEMP CLASS
+	// SOMETHING SHOULD BE DONE WITH
+	// IT AT SOME POINT IN TIME
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
