cfca784bdc1aec2065840f2769ff81e3728b707a
James Berck III <jwberck1@gmail.com>
2016-02-07 03:02:09 -0600

    Player movement is now working! Just put the script on the PlayerObject (still needs collision)

---
+++ TheModeratelyHungryDiabetic/Assets/Script/MovementScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class MovementScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector3 speed = new Vector3(5, 0 , 5);
+
+
+    // 2 - Store the movement
+    private Vector3 movement;
+
+    void Update()
+    {
+        // Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+        // Movement per direction
+        movement = new Vector3( speed.x * inputX, 0, speed.x * inputY);
+
+
+		/*
+        var pos = Camera.main.WorldToScreenPoint(transform.position);
+        var dir = Input.mousePosition - pos;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+*/
+    }
+		
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+		GetComponent<Rigidbody>().velocity = movement;
+    }
+
+}
+
