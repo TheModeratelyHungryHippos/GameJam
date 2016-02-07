4e34c79561dfab489ac188acf8f93e744c79714f
James Berck III <jwberck1@gmail.com>
2016-02-07 12:59:22 -0600

    Camera shake implemented

---
+++ TheModeratelyHungryDiabetic/Assets/Script/CameraShakeScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeScript : MonoBehaviour {
+
+	public GameObject CarbBarSlider;
+	public GameObject Player;
+	public GameObject Camera;
+	Vector3 originalCameraPosition;
+
+	float shakeAmt = 1;
+
+	void Start()
+	{ 
+		//ShakingLevel = CarbBarSlider.GetComponent<CarbBar> ().ShakingMeter;
+		InvokeRepeating ("PeriodicCameraShake", 0, 1);
+	}
+
+	void PeriodicCameraShake() 
+	{
+		//
+		originalCameraPosition = Player.transform.localPosition;
+		shakeAmt =  CarbBarSlider.GetComponent<CarbBar> ().ShakingMeter * .0025f;
+		InvokeRepeating("CameraShake", 0, .01f);
+		Invoke("StopShaking", 0.3f);
+
+	}
+
+	void CameraShake()
+	{
+		if(shakeAmt>0) 
+		{
+			float quakeAmt = Random.value*shakeAmt*2 - shakeAmt;
+			Vector3 pp = Camera.transform.position;
+			pp.x+= quakeAmt;
+			pp.z += quakeAmt;// can also add to x and/or z
+			Camera.transform.position = pp;
+		}
+	}
+
+	void StopShaking()
+	{
+		CancelInvoke("CameraShake");
+		Camera.transform.position = originalCameraPosition;
+	}
+
+
+}
