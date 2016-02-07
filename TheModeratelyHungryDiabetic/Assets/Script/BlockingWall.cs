46a85374b1455629358aaeb481146f1a1e38d7a5
James Berck III <jwberck1@gmail.com>
2016-02-06 21:34:48 -0600

    Added all food and Insulin Items with Parent classes for each type that inherits from PhysObj. Also added Blocking child of PhysObj that is the parent of Wall, UDWall, and LRWall. These are what we need for barebones gameplay. Also added all of these to the stats init.

--- TheModeratelyHungryDiabetic/Assets/Script/BlockingWall.cs
+++ TheModeratelyHungryDiabetic/Assets/Script/BlockingWall.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BlockingWall : ParentPhysObj {
+public class BlockingWall : Blocking {
 
 }
--- TheModeratelyHungryDiabetic/Assets/Script/Stats.cs
+++ TheModeratelyHungryDiabetic/Assets/Script/Stats.cs
@@ -20,12 +20,23 @@ public static class Stats : object {
 		// % No items, % One items, % Two items, % Three items, % Four items
 		GeneratedItemsPercent = new int[5]{1, 1, 1, 1, 1 };
 		GameObjects = new ObjectStats[6];
-		GameObjects [0] = new ObjectStats ("Wall", 0, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
-		GameObjects [1] = new ObjectStats ("Apple", 1, QType.Avoidable, Direction.None, 20, typeof(BlockingWall)); // TODO: Change type
-		GameObjects [2] = new ObjectStats ("Spikes", 2, QType.Avoidable, Direction.None, 20, typeof(BlockingWall));  // TODO: Change type
-		GameObjects [3] = new ObjectStats ("MoveLR", 3, QType.Moving, Direction.LeftRight, 20, typeof(BlockingWall));  // TODO: Change type
-		GameObjects [4] = new ObjectStats ("MoveUP", 4, QType.Moving, Direction.UpDown, 20, typeof(BlockingWall));  // TODO: Change type
-		GameObjects [5] = new ObjectStats ("Full", 5, QType.FullRoom, Direction.None, 20, typeof(BlockingWall));  // TODO: Change type
+		GameObjects [0] = new ObjectStats ("FoodBanana", 0, QType.Avoidable, Direction.None, 20, typeof(FoodBanana));
+		GameObjects [1] = new ObjectStats ("FoodPineapple", 1, QType.Avoidable, Direction.None, 20, typeof(FoodPineapple)); 
+		GameObjects [2] = new ObjectStats ("FoodBigPretzel", 2, QType.Avoidable, Direction.None, 20, typeof(FoodBigPretzel)); 
+		GameObjects [3] = new ObjectStats ("FoodPizza", 3, QType.Avoidable, Direction.None, 20, typeof(FoodPizza)); 
+		GameObjects [4] = new ObjectStats ("InsulinOne", 4, QType.Avoidable, Direction.None, 20, typeof(InsulinOne));  
+		GameObjects [5] = new ObjectStats ("InsulinThree", 5, QType.Avoidable, Direction.None, 20, typeof(InsulinThree));
+		GameObjects [6] = new ObjectStats ("InsulinFive", 6, QType.Avoidable, Direction.None, 20, typeof(InsulinFive));
+		GameObjects [7] = new ObjectStats ("InsulinSeven", 7, QType.Avoidable, Direction.None, 20, typeof(InsulinSeven));
+		GameObjects [8] = new ObjectStats ("BlockingWall", 8, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
+		GameObjects [9] = new ObjectStats ("BlockingUDWall", 9, QType.Moving, Direction.UpDown, 20, typeof(BlockingUDWall));
+		GameObjects [10] = new ObjectStats ("BlockingLRWall", 10, QType.Moving, Direction.LeftRight, 20, typeof(BlockingWall));
+		GameObjects [11] = new ObjectStats ("BlockingWall", 11, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
+		GameObjects [12] = new ObjectStats ("BlockingWall", 12, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
+		GameObjects [13] = new ObjectStats ("BlockingWall", 13, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
+		GameObjects [14] = new ObjectStats ("BlockingWall", 14, QType.Blocking, Direction.None, 20, typeof(BlockingWall));
+
+
 
 		for (int i = 0; i < GameObjects.Length; i++) {
 			KeyMap [GameObjects [i].Name] = i;
